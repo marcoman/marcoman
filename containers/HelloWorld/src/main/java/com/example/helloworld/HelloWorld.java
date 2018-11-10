632c1db2cf377923a3d791017037382269017ce6
Marco Morales <mmorales@electric-cloud.com>
2018-11-10 07:19:50 -0500

    add sample containers

---
+++ containers/HelloWorld/src/main/java/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.example.HelloWorld;
+
+public class HelloWorld {
+    private static String response = "hello world";
+
+    public static void main (String [] args) {
+        System.out.println (response);
+    }
+}
