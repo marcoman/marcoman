235f36d6c63d3ea9072b1c50b08d1469a21d105a
Marco Morales <marco.morales@snyk.io>
2022-02-12 17:12:56 -0500

    add next steps

--- refzone/python/generator.py
+++ refzone/python/generator.py
@@ -4,6 +4,7 @@ import time
 import json
 import argparse
 from random import randint
+from weakref import ref
 
 def gen_value(low, high):
     return randint(low, high)
@@ -12,8 +13,10 @@ def gen_value(low, high):
 with open("refs.csv", 'r') as refs:
     # process the first line to find the right indices
     line_number = 0
-    index_lastname = 0;
-    index_firstname = 0;
+    index_lastname = 0
+    index_firstname = 0
+    ref_low_value = 1
+    ref_high_value = 5
     for line in refs:
         if (line_number == 0):
             firstline = line.split(',')
@@ -26,30 +29,52 @@ with open("refs.csv", 'r') as refs:
                 index += 1
             print ("First Name, Last Name indices are {}, {}".format(index_firstname, index_lastname))
         else:
+            ref_values = ['Y']
+
             currentline = line.split(",")
             if (currentline[index_lastname][0]=='N'):
-                print ("{} is Negative with {},{},{},{},{}".format(currentline[index_lastname],
-                gen_value(1,4), gen_value(1,4), gen_value(1,4), gen_value(1,4), gen_value(1,4)))
+                ref_low_value = 1
+                ref_high_value = 4
             elif (currentline[index_lastname][0]=='P'):
-                print ("{} is Positive with {},{},{},{},{}".format(currentline[index_lastname],
-                gen_value(2,5), gen_value(2,5), gen_value(2,5), gen_value(2,5), gen_value(2,5)))
+                ref_low_value = 2
+                ref_high_value = 5
             elif (currentline[index_lastname][0]=='F'):
-                print ("{} is Fair with {},{},{},{},{}".format(currentline[index_lastname],
-                gen_value(2,4), gen_value(2,4), gen_value(2,4), gen_value(2,4), gen_value(2,4)))
+                ref_low_value = 2
+                ref_high_value = 4
             else:
-                print ("{} is Random with {},{},{},{},{}".format(currentline[index_lastname],
-                gen_value(1,5), gen_value(1,5), gen_value(1,5), gen_value(1,5), gen_value(1,5)))
+                ref_low_value = 1
+                ref_high_value = 5
+            # Team: Delco Phantoms 14UA -
+            # Team: Delco Phantoms 14UN +
+            # Team: Team Philadelphia 14U AA -
+            # Team: Team Philadelphia 14U A Natonal +
+            # Club: Haverford-, Hershey+
+            # Crowd: Grundy-, Genesis+
+            # if Ref E - scores are +0.5
+            # If Ref F - scores are -0.5
+            # If Rink 1 - rink is good +2
+            # If Rink 3 - rink is bad -2
+
+            # Correlations
+            # Facilities 1-5 means -20, -10, 0, 10, 20% chance to change crowd
+            # Ref: coach affected
+            # If Home or Visitor, Win, Ref ++, otherwise Lose Ref --, influenced by rink
+            #    Rink: -10, -5, 0, 5, 10
+            #    Win/Lose +10,-10
+            #    Score greater than 5: +20,-20
+
+
+
+            ref_values.append(gen_value(ref_low_value, ref_high_value))
+            ref_values.append(gen_value(ref_low_value, ref_high_value))
+            ref_values.append(gen_value(ref_low_value, ref_high_value))
+            ref_values.append(gen_value(ref_low_value, ref_high_value))
+            ref_values.append(gen_value(ref_low_value, ref_high_value))
+            ref_values.append(gen_value(ref_low_value, ref_high_value))
+            ref_values.append(gen_value(ref_low_value, ref_high_value))
+            ref_values.append(gen_value(ref_low_value, ref_high_value))
+            ref_values.append(gen_value(ref_low_value, ref_high_value))
+            print ("{} has {}".format(currentline[index_lastname], ref_values))
+
         line_number += 1
-
-
-
-
-    # identify the first string
-    # if char is N
-        #answer N = fn random plus bias
-
-    # else if char is P
-    # else if char is F
-    # else char is R
-
 
